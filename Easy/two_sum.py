425e2fe64a31b52d8103fbadb7b8e1a7f50113ec
pavka <pavka665@gmail.com>
2025-03-20 00:19:19 +0200

    Initial commit and first problem solution in folder Easy

---
+++ Easy/two_sum.py
@@ -0,0 +1,16 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+def two_sum(nums, target):
+    num_to_index = {}
+    for index, num in enumerate(nums):
+        complement = target - num
+        if complement in num_to_index:
+            return [num_to_index[complement], index]
+        num_to_index[num] = index
+
+nums = [3,2,4]
+target = 6
+
+print(two_sum(nums, target))
